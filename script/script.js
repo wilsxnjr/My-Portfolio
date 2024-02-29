8da1968bf636dd71fc5af015b3dd0224a06077b8
Wilson Junior <wilsonmachavajunior@gmail.com>
2024-02-29 18:16:34 -0300

    My Portfolio

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    if(menu.style.transform == "translateX(-100%)") {
+        menu.style.transform = "translateX(0%)";
+    }
+    else {
+        menu.style.transform = "translateX(-100%)";
+    }
+}
